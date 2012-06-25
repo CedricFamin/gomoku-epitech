b02d40b9ae3ddc0e2ef5a9d1032b3f2bdeb7810d
philippe baum <baum_p@pc-baum-p.(none)>
2012-06-25 18:29:25 +0200

    Finished is now way more fun :D

--- gomoku/Finished.cpp
+++ gomoku/Finished.cpp
@@ -1,14 +1,22 @@
 #include "Finished.h"
 #include "ui_Finished.h"
+#include <QPixmap>
+#include <QIcon>
 
 Finished::Finished(QWidget *parent) :
     QDialog(parent),
     ui(new Ui_Finished)
 {
     ui->setupUi(this);
+    this->pixmap = new QPixmap(":/new/prefix1/win.jpg");
+    QIcon icon(*pixmap);
+    QSize iconSize(pixmap->width(), pixmap->height());
+    ui->pushButton->setIconSize(iconSize);
+    ui->pushButton->setIcon(icon);
 }
 
 Finished::~Finished()
 {
+    delete pixmap;
     delete ui;
 }
--- gomoku/Finished.h
+++ gomoku/Finished.h
@@ -15,6 +15,7 @@ public:
 
 private:
     Ui_Finished *ui;
+    QPixmap* pixmap;
 
 };
 
